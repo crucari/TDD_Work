6e750b48e4ed717e22f94c1dac6b61ff7bfb2dd3
Carina Cruz <lecruz@bu.edu>
2018-07-31 23:56:31 +0000

    in progress

--- bowling.c
+++ bowling.c
@@ -1,1 +1,18 @@
-scores = {10,0,10,0,10,0,10,0,10,0,10,0,10,0,10,0,10,0,10,10,10};
+#include <cs50.h>
+#include <stdio.h>
+#include <assert.h>
+
+void tests(void)
+{
+    int five = 5;
+    assert(five == 5 && "five should contain the int 5");
+
+}
+
+int main(void)
+{
+    tests();
+}
+// scores = {10,0,10,0,10,0,10,0,10,0,10,0,10,0,10,0,10,0,10,10,10};
+
+//a empty string is false
